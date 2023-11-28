da63e331a0ad649099ef3c4acbe5e0c989c5fb74
nakulpadalkar <2807210+nakulpadalkar@users.noreply.github.com>
2023-11-27 23:35:48 -0500

    added map to the html

--- AmesDashboard/AmesDashboard.py
+++ AmesDashboard/AmesDashboard.py
@@ -1,12 +1,20 @@
+import os
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import RobustScaler
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify,render_template
 from src.AmesFeatureEngineer import AmesFeatureEngineer as AFE
+from dotenv import load_dotenv
+
+load_dotenv()  # Load environment variables from .env file
 
 app = Flask(__name__)
-engineer = AmesFeatureEngineer()
+engineer = AFE()
+
+@app.route('/')
+def index():
+    return render_template('index.html', google_maps_api_key=os.getenv('GOOGLE_MAPS_API_KEY'))
 
 @app.route('/predict', methods=['POST'])
 def predict():
